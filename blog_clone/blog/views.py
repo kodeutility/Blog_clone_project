fbb88db8350940ad1c908322e72e6f4c0a3fbe40
Kiran <kodeutility@gmail.com>
2019-02-09 22:50:13 +0530

    LOGOUT_REDIRECT_URL added in settings.py

--- blog_clone/blog/views.py
+++ blog_clone/blog/views.py
@@ -57,7 +57,7 @@ def add_comment_to_post(request,pk):
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
-            comment = form.save(commit=Flase)
+            comment = form.save(commit=False)
             comment.post = post
             comment.save()
             return redirect('post_detail',pk=post.pk)
