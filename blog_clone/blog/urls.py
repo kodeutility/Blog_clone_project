a6a56ee3357dec04c85a36d3ce4a661ea91f6c50
Kiran <kodeutility@gmail.com>
2019-02-09 13:08:32 +0530

    CreatePostView, UpdatePostViwe, DeletePostView, DraftListView are created in views.py. Appropriate urls are created in urls.py.

--- blog_clone/blog/urls.py
+++ blog_clone/blog/urls.py
@@ -4,5 +4,9 @@ from blog import views
 urlpatterns = [
     path('',views.PostListView.as_view(),name='post_list'),
     path('about/',views.AboutView.as_view(),name='about'),
-    path('post/',views.PostDetailView.as_view(),name='post_detail'),
+    path('post/<pk>/',views.PostDetailView.as_view(),name='post_detail'),
+    path('post/new/',views.CreatePostView.as_view(),name='new_post'),
+    path('post/<pk>/edit/',views.UpdatePostView.as_view(),name='edit_post'),
+    path('post/<pk>/delete/',views.DeletePostView.as_view(),name='delete_post'),
+    path('drafts/',views.DraftListView.as_view(),name='draft_list'),
 ]
--- blog_clone/blog/views.py
+++ blog_clone/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from blog.models import Comment,Post
+from blog.forms import PostForm,CommentForm
 from django.utils import timezone
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView,ListView,DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 
 # Create your views here.
 class AboutView(TemplateView):
@@ -15,3 +19,27 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_model = PostForm
+    model = Post
+
+class UpdatePostView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_model = PostForm
+    model = Post
+
+class DeletePostView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(publication_date__isnull=True).order_by('create_date')
