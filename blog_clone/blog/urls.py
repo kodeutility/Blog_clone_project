ba611802651d70a740360a76dded98f8d79b9aa7
Kiran <kodeutility@gmail.com>
2019-02-09 13:48:41 +0530

    post_publish, add_comment_to_post, comment_approve, comment_remove view functions are added

--- blog_clone/blog/urls.py
+++ blog_clone/blog/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('post/<pk>/edit/',views.UpdatePostView.as_view(),name='edit_post'),
     path('post/<pk>/delete/',views.DeletePostView.as_view(),name='delete_post'),
     path('drafts/',views.DraftListView.as_view(),name='draft_list'),
+    path('post/<pk>/comment/',views.add_comment_to_post,name='add_comment_to_post'),
+    path('comment/<pk>/approve/',views.comment_approve,name='comment_approve'),
+    path('comment/<pk>/remove/',views.comment_remove,name='comment_remove'),
+    path('post/<pk>/publish/',views.post_publish,name='post_publish'),
+
 ]
